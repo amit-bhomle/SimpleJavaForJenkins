fb8e0a875b85763d7f06201a9b2e0b2e3e8fc8c4
Amit <amit.bhomle@gmail.com>
2023-08-18 12:35:55 +0530

    Changes

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+public class ReverseString {
+	public static void main(String[] args){
+
+    int num = 2023, reversed = 0;
+    
+    System.out.println("Original Number is : " + num);
+
+    // run loop until num becomes 0
+    while(num != 0) {
+    
+      // get last digit from num
+      int digit = num % 10;
+      reversed = reversed * 10 + digit;
+
+      // remove the last digit from num
+      num /= 10;
+    }
+
+    System.out.println("Reversed Number is : " + reversed);
+  }
+}

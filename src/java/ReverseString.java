705d14810d66296b2c42380d4c6ae416e997f1b4
onlytoamit <70512802+onlytoamit@users.noreply.github.com>
2023-08-18 12:06:14 +0530

    Update ReverseString.java

--- src/java/ReverseString.java
+++ src/java/ReverseString.java
@@ -1,7 +1,7 @@
 public class ReverseString {
 	public static void main(String[] args){
 
-    int num = 1982, reversed = 0;
+    int num = 1980, reversed = 0;
     
     System.out.println("Original Number is : " + num);
 
